bb334b614e305f355a5e96ad9f5d730895564575
Mario Adrian <many.mario@gmx.de>
2021-01-09 15:11:11 +0100

    update

--- src/app/sonos-api.ts
+++ src/app/sonos-api.ts
@@ -1,5 +1,43 @@
 export interface SonosApiConfig {
-    server: string;
-    port: string;
-    rooms: string[];
+  server: string;
+  port: string;
+  rooms: string[];
+}
+
+export interface SonosApiTrack {
+  artist: string;
+  title: string;
+  album: string;
+  albumArtURI: string;
+  duration: number;
+  uri: string;
+}
+export interface SonosApiState {
+  currentTrack: SonosApiTrack;
+  nextTrack: SonosApiTrack;
+  volume: number;
+  mute: boolean;
+  trackNo: number;
+  elapsedTime: number;
+  elapsedTimeFormatted: string;
+  zoneState: string;
+  playerState: string | "PLAYING" | "PAUSED_PLAYBACK";
+  playMode: {
+    shuffle: boolean;
+    repeat: "none" | "all" | "one";
+    crossfade: boolean;
+  };
+  equalizer: {
+    bass: number;
+    treble: number;
+    loudness: boolean;
+  };
+}
+
+export interface SonosApiQueue {
+  uri?: string;
+  albumArtURI: string;
+  title: string;
+  artist: string;
+  album: string;
 }
